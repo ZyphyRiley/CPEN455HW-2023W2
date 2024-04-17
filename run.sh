a9df2588415aa8efd62dfe287ad2e49c29ce42b9
Riley Zhang <60052045+ZyphyRiley@users.noreply.github.com>
2024-04-16 17:00:17 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 python pcnn_train.py \
 --batch_size 16 \
 --sample_batch_size 192 \
---sampling_interval 1 \
---save_interval 1 \
+--sampling_interval 8 \
+--save_interval 8 \
 --dataset cpen455 \
 --nr_resnet 1 \
 --nr_filters 20 \
