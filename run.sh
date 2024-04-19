98c43373e128fc4d984638207ed96a5f8c421890
ZyphyRiley <ryyizg8@gmail.com>
2024-04-19 12:15:55 -0700

    sampling again

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ python pcnn_train.py \
 --sampling_interval 8 \
 --save_interval 8 \
 --dataset cpen455 \
---nr_resnet 1 \
+--nr_resnet 2 \
 --nr_filters 20 \
 --nr_logistic_mix 3 \
 --lr_decay 0.999995 \
