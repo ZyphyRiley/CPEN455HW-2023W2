4a07336160b578d151bdcfb72e85224bdaac97d9
ZyphyRiley <ryyizg8@gmail.com>
2024-04-18 22:38:26 -0700

    more prints

--- model.py
+++ model.py
@@ -29,6 +29,7 @@ class AbsolutePositionalEncoding(nn.Module):
         # Use indices to slice self.W for each row in x
         positional_encodings = self.W[indices]  # Shape: B x D
         # positional_encodings now holds [[self.W[0,:]], [self.W[1,:]], ..., [self.W[B-1,:]]]
+        print("positional_encodings: ", positional_encodings.shape)
         
         # Add positional encodings to input x
         out = x + positional_encodings
