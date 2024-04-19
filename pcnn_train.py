68c1144d9bf2d808e7d70a62862588285cf02076
ZyphyRiley <ryyizg8@gmail.com>
2024-04-18 23:39:15 -0700

    run.sh change

--- pcnn_train.py
+++ pcnn_train.py
@@ -13,8 +13,6 @@ from pprint import pprint
 import argparse
 from pytorch_fid.fid_score import calculate_fid_given_paths
 
-global_label = ()
-
 def train_or_test(model, data_loader, optimizer, loss_op, device, args, epoch, mode = 'training'):
     if mode == 'training':
         model.train()
@@ -28,7 +26,6 @@ def train_or_test(model, data_loader, optimizer, loss_op, device, args, epoch, m
         model_input, label = item # modified to take label and pass it to the model
         model_input = model_input.to(device)
         model_output = model(model_input, label)
-        global_label = label
         loss = loss_op(model_input, model_output)
         loss_tracker.update(loss.item()/deno)
         if mode == 'training':
