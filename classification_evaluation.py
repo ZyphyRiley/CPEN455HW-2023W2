79de49b0292592da2ec30eb8941f49ab77467f1e
ZyphyRiley <ryyizg8@gmail.com>
2024-04-19 21:39:05 -0700

    mod

--- classification_evaluation.py
+++ classification_evaluation.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
     model = model.to(device)
     #Attention: the path of the model is fixed to 'models/conditional_pixelcnn.pth'
     #You should save your model to this path
-    model.load_state_dict(torch.load('models/conditional_pixelcnn.pth'))
+    model.load_state_dict(torch.load('models/conditional_pixelcnn.pth', map_location=device)) # MODIFICATION TO RUN ON CPU IF TRAINED ON GPU
     model.eval()
     print('model parameters loaded')
     acc = classifier(model = model, data_loader = dataloader, device = device)
