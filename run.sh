88986fe05443a33c88a76af5781cf1f299272cb0
ZyphyRiley <ryyizg8@gmail.com>
2024-04-19 17:41:47 -0700

    almost final params

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 python pcnn_train.py \
 --batch_size 16 \
 --sample_batch_size 192 \
---sampling_interval 16 \
---save_interval 16 \
+--sampling_interval 8 \
+--save_interval 8 \
 --dataset cpen455 \
 --nr_resnet 2 \
---nr_filters 20 \
---nr_logistic_mix 3 \
+--nr_filters 40 \
+--nr_logistic_mix 5 \
 --lr_decay 0.999995 \
---max_epochs 200 \
+--max_epochs 8 \
 --en_wandb False \
