76d1def722953840896ad64f03e373af234cad49
ZyphyRiley <ryyizg8@gmail.com>
2024-04-19 17:45:17 -0700

    final runthrough attempt

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 python pcnn_train.py \
 --batch_size 16 \
 --sample_batch_size 192 \
---sampling_interval 8 \
---save_interval 8 \
+--sampling_interval 256 \
+--save_interval 128 \
 --dataset cpen455 \
 --nr_resnet 2 \
 --nr_filters 40 \
 --nr_logistic_mix 5 \
 --lr_decay 0.999995 \
---max_epochs 8 \
+--max_epochs 256 \
 --en_wandb False \
