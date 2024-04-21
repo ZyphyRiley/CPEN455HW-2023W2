7b4a05106f4ee671bd478f9aebf8d350beae4213
ZyphyRiley <ryyizg8@gmail.com>
2024-04-21 02:39:20 -0700

    sleep to delay?

--- test_classification.py
+++ test_classification.py
@@ -16,6 +16,7 @@ import argparse
 NUM_CLASSES = len(my_bidict)
 import csv
 import os
+import time
 
 
 def get_label_logits(model, model_input, device):
@@ -34,7 +35,8 @@ def classify_and_submit(model, data_loader, device):
         model_input, categories = item
         model_input = model_input.to(device)
         answer, logits = get_label_logits(model, model_input, device)
-        
+        time.sleep(1)
+
         logits.tolist()
         full_logits.extend(logits)
         full_answers.extend(answer)
